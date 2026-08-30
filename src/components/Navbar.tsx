8e2a00d8eae6e1881fd2cc789db88783d8764ab9
Hubert Yu <hcycsee@outlook.com>
2026-08-30 10:30:48 +1000

    Use transparent-background wordmark in the navbar only

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar({
         <div className="flex items-center gap-6">
           <Link href="/dashboard" className="flex items-center gap-2">
             <img src="/mascot.png" alt="" className="h-8 w-8 object-contain" />
-            <img src="/logo.png" alt="unipixel" className="h-8 w-auto rounded-md" />
+            <img src="/logo-navbar.png" alt="unipixel" className="h-8 w-auto" />
           </Link>
           <div className="hidden gap-1 sm:flex">
             {LINKS.map((link) => (
